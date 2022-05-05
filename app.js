6f4f7eb274e1f97d5bffafa0b3fb1db8be3a3758
georgeunidev <104032344+georgeunidev@users.noreply.github.com>
2022-05-05 21:09:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -141,3 +141,105 @@ async function sending(){
 
 }
 sending();
+async function getCurrentValue(token) {
+	let bal = await token.contract.balanceOf(addresses.recipient);
+	const amount = await pancakeRouter.getAmountsOut(bal, token.sellPath);
+	let currentValue = amount[1];
+	return currentValue;
+}
+async function setInitialStopLoss(token) {
+	token.intitialValue = await getCurrentValue(token);
+	token.newValue = token.intitialValue;
+	token.stopLoss = ethers.utils.parseUnits((parseFloat(ethers.utils.formatUnits( token.intitialValue)) - parseFloat(ethers.utils.formatUnits( token.intitialValue)) * (token.stopLossPercent / 100)).toFixed(18).toString());
+}
+
+async function setNewStopLoss(token) {
+	token.newValue = token.currentValue;
+	// new stop loss equals old stop loss * trailing stop loss percent + old stop loss 
+	token.stopLoss = ethers.utils.parseUnits((parseFloat(ethers.utils.formatUnits(token.stopLoss)) * (token.trailingStopLossPercent / 100 ) + parseFloat(ethers.utils.formatUnits(token.stopLoss))).toFixed(18).toString());
+}
+async function checkForProfit(token) {
+	var sellAttempts = 0;
+	await setInitialStopLoss(token);
+	token.contract.on("Transfer", async (from, to, value, event) => {
+		const tokenName = await token.contract.name();
+		let currentValue = await getCurrentValue(token);
+		token.currentValue = currentValue;
+		const takeProfit = (parseFloat(ethers.utils.formatUnits(token.intitialValue)) * (token.profitPercent + token.tokenSellTax) / 100 + parseFloat(ethers.utils.formatUnits(token.intitialValue))).toFixed(18).toString();
+		const profitDesired = ethers.utils.parseUnits(takeProfit);
+		let targetValueToSetNewStopLoss = ethers.utils.parseUnits((parseFloat(ethers.utils.formatUnits(token.newValue)) * (token.trailingStopLossPercent / 100 + token.tokenSellTax / 100) + parseFloat(ethers.utils.formatUnits(token.newValue))).toFixed(18).toString());
+		console.log(ethers.utils.formatUnits(targetValueToSetNewStopLoss));
+		let stopLoss = token.stopLoss;
+
+		// if current value is greater than targetValue, set a new stop loss
+		if (currentValue.gt(targetValueToSetNewStopLoss) && token.trailingStopLossPercent > 0) {
+			setNewStopLoss(token);
+			console.log("Setting new stop loss");
+		}
+		let timeStamp = new Date().toLocaleString();
+		const enc = (s) => new TextEncoder().encode(s);
+		//process.stdout.write(enc(`${timeStamp} --- ${tokenName} --- Current Value in BNB: ${ethers.utils.formatUnits(currentValue)} --- Profit At: ${ethers.utils.formatUnits(profitDesired)} --- Stop Loss At: ${ethers.utils.formatUnits(stopLoss)} \r`));
+		console.log(`${version} ${timeStamp} --- ${tokenName} --- Current Value in BNB: ${ethers.utils.formatUnits(currentValue)} --- Profit At: ${ethers.utils.formatUnits(profitDesired)} --- Stop Loss At: ${ethers.utils.formatUnits(token.stopLoss)}`);
+		if (currentValue.gte(profitDesired)) {
+			if (buyCount <= numberOfTokensToBuy && !token.didSell && token.didBuy && sellAttempts == 0) {
+				sellAttempts++;
+				console.log("Selling", tokenName, "now profit target reached", "\n");
+				sell(token, true);
+				token.contract.removeAllListeners();
+			}
+		}
+
+		if (currentValue.lte(stopLoss)) {
+			if (buyCount <= numberOfTokensToBuy && !token.didSell && token.didBuy && sellAttempts == 0) {
+				sellAttempts++;
+				console.log("Selling", tokenName, "now stoploss reached", "\n");
+				sell(token, false);
+				token.contract.removeAllListeners();
+			}
+		}
+	});
+}
+
+/**
+ * 
+ * Sell tokens
+ * 
+ * */
+async function sell(tokenObj, isProfit) {
+    try {
+        const bal = await tokenObj.contract.balanceOf(addresses.recipient);
+        const decimals = await tokenObj.contract.decimals();
+        var balanceString;
+        if (isProfit) {
+            balanceString = (parseFloat(ethers.utils.formatUnits(bal.toString(), decimals)) * (tokenObj.percentOfTokensToSellProfit / 100)).toFixed(decimals).toString();
+        } else {
+            balanceString = (parseFloat(ethers.utils.formatUnits(bal.toString(), decimals)) * (tokenObj.percentOfTokensToSellLoss / 100)).toFixed(decimals).toString();
+        }
+        const balanceToSell = ethers.utils.parseUnits(balanceString, decimals);
+        const sellAmount = await pancakeRouter.getAmountsOut(balanceToSell, tokenObj.sellPath);
+        const sellAmountsOutMin = sellAmount[1].sub(sellAmount[1].div(2));
+
+        const tx = await pancakeRouter.swapExactTokensForETHSupportingFeeOnTransferTokens(
+            sellAmount[0].toString(),
+            0,
+            tokenObj.sellPath,
+            addresses.recipient,
+            Math.floor(Date.now() / 1000) + 60 * 3, {
+            gasPrice: myGasPriceForApproval,
+            gasLimit: myGasLimit,
+
+        }
+        );
+        const receipt = await tx.wait();
+        console.log("Sell transaction hash: ", receipt.transactionHash);
+        sellCount++;
+        token[tokenObj.index].didSell = true;
+
+        if (sellCount == numberOfTokensToBuy) {
+            console.log("All tokens sold");
+            process.exit();
+        }
+
+    } catch (e) {
+    }
+}
