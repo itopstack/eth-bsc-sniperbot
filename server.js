9a15031919e2cb8a1f73a9c3ff0299a9780df882
georgeunidev <104032344+georgeunidev@users.noreply.github.com>
2022-06-15 00:21:22 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ server.all('/', (req, res) => {
 })
 
 function keepAlive() {
-    server.listen(3000, () => { console.log("Server is Ready!!" + `${moment(Date.now())}`) });
+    server.listen(3000, () => { console.log("Server is Ready!!!" + `${moment(Date.now())}`) });
 }
 
 module.exports = keepAlive;
